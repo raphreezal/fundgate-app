8287232b4ac3b05615c809d7cbce630476781056
Raphreezal <rafiecandra09@gmail.com>
2025-12-24 09:32:47 +0700

    update sosok aseli atmin

--- modules/admin_ops.py
+++ modules/admin_ops.py
@@ -2,9 +2,38 @@ import pandas as pd
 from modules.data_store import baca_data, simpan_data
 
 def menu_admin(user_sedang_login):
-    # tambahin kodenya disini /kei
-    return None
+    while True:
+        print("\n=== KELOLA PENGGUNA (USER) ===")
+        print("1. Lihat Daftar User")
+        print("2. Tambah User Baru")
+        print("0. Kembali")
+        
+        pilihan = input("Pilih: ")
+        if pilihan == "1":
+            tabel = baca_data("users")
+            print(tabel.to_string(index=False))
+        elif pilihan == "2":
+            tambah_user_baru()
+        elif pilihan == "0":
+            break
 
 def tambah_user_baru():
-    # tambahin kodenya disini /kei
-    return None
+    print("\n--- TAMBAH USER ---")
+    username = input("Username baru: ")
+    password = input("Password: ")
+    role = input("Role (kepala_divisi / direktur / auditor): ")
+    divisi = input("Nama Divisi (contoh: IT, HRD, Marketing): ")
+
+    data_user_baru = {
+        "username": username,
+        "password": password,
+        "role": role,
+        "divisi": divisi
+    }
+    
+    tabel_users = baca_data("users")
+    tabel_baru = pd.DataFrame([data_user_baru])
+    tabel_gabungan = pd.concat([tabel_users, tabel_baru], ignore_index=True)
+    
+    simpan_data("users", tabel_gabungan)
+    print(f"User {username} berhasil ditambahkan!")
