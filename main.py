2b0ce9bcb2ad6eb93fdf2a5287120a2776d7c67e
najwaAK16 <aidatulkaromah@gmail.com>
2026-01-02 02:46:10 +0700

    memperbaiki fitur create user pada menu manajer keuangan

--- main.py
+++ main.py
@@ -63,7 +63,6 @@ def main():
         if data_user is None:
             print("Gagal! Username atau Password salah.")
             print("Gagal! Username atau Password salah.")
-            print("Gagal! Username atau Password salah.")git 
         else:
             # kalau berhasil login, cek rolenya siapa /kei
             nama = data_user['username']
