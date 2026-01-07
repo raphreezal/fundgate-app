cdb6d7e57a4cf293826024db5003fd1168286e1a
najwaAK16 <aidatulkaromah@gmail.com>
2026-01-07 14:50:37 +0700

    fix conflict auth dan main

--- modules/auth.py
+++ modules/auth.py
@@ -1,8 +1,6 @@
 import re
-<<<<<<< Updated upstream
 from modules.utility import baca_data
 
-# login revisi / najwa
 def proses_login(username, password):
     tabel = baca_data("users")
 
@@ -17,6 +15,7 @@ def proses_login(username, password):
 
 def cek_username(username):
     tabel = baca_data("users")
+
     if tabel.empty:
         return "USERNAME_TIDAK_ADA"
 
@@ -24,78 +23,3 @@ def cek_username(username):
         return "USERNAME_TIDAK_ADA"
 
     return "ADA"
-
-
-# def proses_login(username_input, password_input):
-#     # Ambil data dari CSV / najwa
-#     tabel_users = baca_data("users")
-#     username_input = username_input.lower()
-#     list_user = tabel_users.to_dict('records') 
-    
-#     user_ditemukan = None
-    
-#     # Linear Search untuk cari username / najwa
-#     for user in list_user:
-#         if str(user['username']).lower() == username_input:
-#             user_ditemukan = user
-#             break 
-
-#     if user_ditemukan is None:
-#         print("Username salah atau tidak terdaftar!")
-#         return None
-#     else:
-#         if str(user_ditemukan['password']) == password_input:
-#             return user_ditemukan 
-#         else:
-#             print("Password salah!")
-#             return None
-=======
-from modules.data_store import baca_data
-
-# validasi password / najwa
-def validasi_password(password):
-    if len(password) < 8:
-        return False, "Password minimal 8 karakter!"
-    if not re.search(r"[A-Z]", password):
-        return False, "Password harus mengandung huruf kapital!"
-    if not re.search(r"[a-z]", password):
-        return False, "Password harus mengandung huruf kecil!"
-    if not re.search(r"[0-9]", password):
-        return False, "Password harus mengandung angka!"
-    
-    simbol = "!@#$%^&*()-_=+[{]};:'\",<.>/?\\|"
-    punya_simbol = False
-    for char in password:
-        if char in simbol:
-            punya_simbol = True
-            break
-    
-    if not punya_simbol:
-        return False, "Password harus mengandung simbol!"
-    
-    return True, "Valid"
-
-def proses_login(username_input, password_input):
-    # Ambil data dari CSV / najwa
-    tabel_users = baca_data("users")
-    username_input = username_input.lower()
-    list_user = tabel_users.to_dict('records') 
-    
-    user_ditemukan = None
-    
-    # Linear Search untuk cari username / najwa
-    for user in list_user:
-        if str(user['username']).lower() == username_input:
-            user_ditemukan = user
-            break 
-
-    if user_ditemukan is None:
-        print("Username salah atau tidak terdaftar!")
-        return None
-    else:
-        if str(user_ditemukan['password']) == password_input:
-            return user_ditemukan 
-        else:
-            print("Password salah!")
-            return None
->>>>>>> Stashed changes
